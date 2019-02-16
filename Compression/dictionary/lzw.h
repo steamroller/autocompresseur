e6a282c5c9d866bb26002362dd65817be9f2381b
Antoine-Fournier <antoine1.fournier@epita.fr>
2019-02-16 15:51:43 +0100

    quelques warning mais je regelerai ca
    Signed-off-by: Antoine-Fournier <antoine1.fournier@epita.fr>

--- Compression/dictionary/lzw.h
+++ Compression/dictionary/lzw.h
@@ -6,5 +6,14 @@ struct dictionary* add(char* to_add,struct dictionary *dico);
 size_t leng(char *s);
 int are_equal(char *s1, char *s2);
 int weight(char *s);
+int is_belonging(struct dictionary *dico, char *s);
+//int max_word(struct dictionary *dico, char *s);
+void *substring(char *s);
+char *final(struct dictionary *dico, char *s,int nbw);
+int digit_count(int nombre);
+char *recup(struct dictionary *dic, int index);
+char *first(char *s);
+char *decompress(char *init,char *dest,int bw);
+int nbword(char *s);
 
 #endif
--- Compression/dictionary/main.c
+++ Compression/dictionary/main.c
@@ -13,72 +13,101 @@ struct dictionary
 };
 
 
-
-void print_array(int *array, size_t length)
+int main(int argc, char *argv[])
 {
-	for(size_t i = 0; i < length; i++)
-		printf("%i ", array[i]);
-	printf("\n");
-}
 
-size_t length(char*s)
-{
-	size_t i = 0;
-	while(*s!=0)
-	{
-		i += 1;
-		s += 1;
-	}
-	return i;
-}
-
-int main()
-{
+	//char p[9]="chocolat";
+	//printf("%i est le poids de chocolat \n",weight(p));
+	//substring(p);
+	//printf("%s\n",p);
 	/*char *u = malloc(1*sizeof(char));
 	*u = 'f';
 	u += 1;
 	*u = 'f';
 	u-=1;
-
+	
 	//printf("%s\n",u);*/
 
-	char tt[8] = "antoine";
-	char ttt[4] = "45";
+	//char tt[8] = "an";
+	//char ttt[4] = "45";
+	//int y = 233;
+	//char *s = (char*)y;
+	//printf("s = %s \n",s);
 
-	
-	struct dictionary *tab = build();
+	//ICI
+	//printf("5 en char donne : %c\n",(char)514);
+	struct dictionary *ret = build();
+	struct dictionary *c = ret;
+	struct dictionary *d = build();
+	//struct dictionary *d = ret;
+	//struct dictionary *c = ret;////LAAA
 
-	struct dictionary *b = add(ttt,tab);
 
-	struct dictionary *ret = add(tt,b);
+	//struct dictionary *m = add("ab",ret);
+	//struct dictionary *c = add("ba",m);
+	/*struct dictionary *b = add(ttt,tab);
+	struct dictionary *c = add("chou",b);
+	struct dictionary *d = add("cho",c);
+	struct dictionary *e = add("anto",d);
+	struct dictionary *u = add("antoinette",e);
+	struct dictionary *ret = add(tt,u);
 	struct dictionary *f = ret;
-	size_t i = 0;
-	while(ret!=NULL)
+	struct dictionary *g = ret;*/
+
+
+	/*size_t i = 0;
+	while(b!=NULL)
 	{
 		//printf("pipi\n");
-		printf("Dic[%zu] = %s, numeric value : %i \n",i,ret->value, ret->num);
-		ret = ret->next;
+		printf("Dic[%zu] = %s, numeric value : %i \n",i,b->value, b->num);
+		b = b->next;
 		i += 1;
 
 
 
-	}
-	while(f != NULL)
+	}*/
+	/*while(f != NULL)
 	{
 		struct dictionary *temp = f;
 		f = f->next;
 		free(temp->value); 
 		free(temp);
 	}
-	free(tab);
+	free(tab);*/
 
-	char un[23] = "je ne suis pas méchant";
-	char deux[23] = "je ne suis pas méchant";
-
+	//char un[22] = "je ne suis pas méchant";
+	//char deux[22] = "je ne suis pas méchant";
+	//char ptn[15] = "45";
+	//printf("an appartient : %i \n",is_belonging(c,"an"));
 	//printf("the len is %zu \n", leng(tt));
 	//printf(" are equal : %i \n",are_equal(un,deux));
 	//printf("the weight of tt is : %i \n",weight(tt));
 
+
+	//printf("max de 454584589 = %i \n" , max_word(c,"chou de choupichou deux"));
+	//printf("Un chasseur sachant chasser doit savoir chasser sans son chien\n");
+	//printf("Resultat apres compression : %s\n",(final(d,argv[1])));
+	//printf("ab appartient : %i \n",is_belonging(c,"ab"));
+	//printf("%zu\n", leng("chou de choupichou deux chou de choupichou deux chou de choupichou deux chou de choupichou deux chou de choupichou deux chou de choupichou deux chou de choupichou deux chou de choupichou deux chou de choupichou deux chou de choupichou deux\n"));
+	//printf("antoine appartient ? : %i \n",is_belonging(b,"antoine"));
+	//printf("max de 456 = %i \n" , max_word(b,"456"));
+	//struct dictionary *test = c;
+	//size_t v = 0;
+	/*while(c!=NULL)
+	{
+		//printf("pipi\n");
+		printf("Dic[%zu] = %s, numeric value : %i \n",v,c->value, c->num);
+		c = c->next;
+		v += 1;
+	}*/
+
+	//printf("%i\n",strcmp("antoine","an"));
+	//printf("en pos 129, il y a : %s\n",recup(test, 129));
+	//printf("sizeofdico = %i\n",sod(test));
+	//printf("en pos 147, il y a %s%s \n",recup(c,147),"ha\n");
+	//printf("eho");
+	
+	
 	//printf("%zu \n",leng(u));
 	/*char* str = malloc(3*sizeof(char));
 	*str = 'f';
@@ -96,11 +125,44 @@ int main()
 		printf("%s \n",str);
 
 	}*/
+	//printf("wtf");
+	//printf("<119><104><111><032><097><114><101><032><121><111><117>\n");
+	//char t[1250] ="<101><104><111><128><111>\n";
 
-
-
+	char *try = calloc(1,200*sizeof(char));
+	//printf("crochet = %s\n",first(t));
+	//printf("decomp = %s\n",decompress(t,try));
 	/*char *phrase = "Bonjour";
 	size_t l = length(phrase);
 	int *array = invalues(phrase,l);
 	print_array(array,l);*/
+	char *tty = calloc(1,888*sizeof(char));
+	tty=final(ret,argv[1],1);
+
+	size_t v = 0;
+	while(c!=NULL)
+	{
+		//printf("pipi\n");
+		printf("Dic[%zu] = %s, numeric value : %i \n",v,c->value, c->num);
+		c = c->next;
+		v += 1;
+	}
+	int n = 0;
+	printf("Initial sentence : %s\n",argv[1]);
+	printf("Resultat apres compression : %s\n",(final(d,argv[1],n)));
+	printf("%i * %i = %i bits\n",nbword(tty),7+(sod(d)/127),nbword(tty)*(7+(sod(d)/127)));
+	printf("Resultat apres decompression : %s\n",decompress(tty,try,nbword));
+	printf("%i * %i = %i bits\n",strlen(argv[1]),7,7*strlen(argv[1]));
+	//printf("nbword = %i\n",nbword(tty));
+
+
+	/*size_t v = 0;
+	while(c!=NULL)
+	{
+		//printf("pipi\n");
+		printf("Dic[%zu] = %s, numeric value : %i \n",v,c->value, c->num);
+		c = c->next;
+		v += 1;
+	}*/
+	//printf(recup(d,130));
 }
